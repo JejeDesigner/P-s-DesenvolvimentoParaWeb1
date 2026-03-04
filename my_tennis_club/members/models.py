7482707837ddb05dc097948cf3f09c852eeca73f
Jennifer Pereira de Almeida <jennifer_je.je@hotmail.com>
2026-03-03 21:36:43 -0300

    Add files via upload
    Atualização Aula dia 03-03-2026

--- my_tennis_club/members/models.py
+++ my_tennis_club/members/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Member(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    phone = models.IntegerField(null=True)
+    joined_date = models.DateField(null=True)
+
--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import HttpResponse
 from django.template import loader
+from .models import Member
 
 def members(request):
-    template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
+    my_members = Member.objects.all().values()
+    template = loader.get_template('all_members.html')
+    context = {
+        'members': my_members,
+    }
+    return HttpResponse(template.render(context, request))
+
+def details(request, id):
+    my_members = Member.objects.get (id=id)
+    template = loader.get_template('details.html')
+    context = {
+        'my_members': my_members,
+    }
+    return HttpResponse(template.render(context, request))
+
+
